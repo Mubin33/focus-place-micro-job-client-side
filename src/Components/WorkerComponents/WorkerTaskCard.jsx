8458c677eaeee390b3287c7ea483f46824ce484c
mubin <mubinulislam14@gmail.com>
2025-01-15 03:38:24 +0600

    worker all task list completed

---
+++ src/Components/WorkerComponents/WorkerTaskCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa";
+
+const WorkerTaskCard = ({ item }) => {
+  const {
+    _id,
+    completion_date,
+    buyerName,
+    task_title,
+    required_workers,
+    perTaskAmount,
+    taskImage,
+  } = item;
+ 
+  const today = new Date().toISOString().split("T")[0];
+ 
+  const isHidden = new Date(completion_date) < new Date(today) || required_workers === 0;
+ 
+  if (isHidden) return null;
+
+  return (
+    <Link>
+      <div className='p-2 shadow-md shadow-green-500 rounded-xl h-full'>
+        <div className='h-32 rounded-xl border-[1px] border-sky-400'>
+          <img className='h-full w-full rounded-xl' src={taskImage} alt="" />
+        </div>
+        <p className='text-sm text-center mt-2 font-semibold'>{task_title}</p>
+        <p className='text-xs text-gray-500'>{buyerName}</p>
+        <p className='space-x-5 mt-2 text-xs font-semibold '>
+          <span>Value: {" "}{perTaskAmount}$</span>
+          <span>Remaining: {" "}{required_workers}</span>
+        </p>
+        <p className='text-xs font-semibold text-gray-600'>Last Date: {" "}{completion_date}</p>
+        <div className='flex justify-end px-3'><FaArrowRight /></div>
+      </div>
+    </Link>
+  );
+};
+
+export default WorkerTaskCard;
--- src/Pages/DashboardPages/WorkerPages/WorkerTaskList.jsx
+++ src/Pages/DashboardPages/WorkerPages/WorkerTaskList.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import WorkerTaskCard from '../../../Components/WorkerComponents/WorkerTaskCard';
+import Loading from '../../../Components/Loading/Loading';
 
 const WorkerTaskList = () => {
+
+    const axiosSecure = useAxiosSecure()
     
+        const { data:allTask=[], isPending ,refetch  } = useQuery({
+            queryKey: ['allTask' ], 
+            queryFn: async() =>{
+                const {data} = await axiosSecure.get(`/task`)
+                return data
+            }
+          })
+          if(isPending) return <Loading/>
+
     return (
-        <div>
-            worker TaskList
+        <div className='grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-5 md:px-7 lg:px-4 px-3'>
+            {
+                allTask.map(item=> <WorkerTaskCard key={item._id} item={item}/>)
+            }
         </div>
     );
 };
