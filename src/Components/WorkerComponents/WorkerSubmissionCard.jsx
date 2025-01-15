a8ce44f5dd3ae4a5df9e6d1b4916a8e70dc245a3
mubin <mubinulislam14@gmail.com>
2025-01-15 20:42:23 +0600

    added a new page like worker my submission page

---
+++ src/Components/WorkerComponents/WorkerSubmissionCard.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const WorkerSubmissionCard = ({ item, refetch }) => {
+  const {
+    _id,
+    task_id,
+    task_title,
+    perTaskAmount,
+    worker_email,
+    submission_details,
+    worker_name,
+    buyerEmail,
+    buyerName,
+    current_date,
+    status,
+  } = item;
+
+  return (
+    <tr className="border-t-[1px] md:border-none ">
+      <td>
+        <div className="flex items-center gap-1 md:gap-3">
+          <div>
+            <div className="text-[10px] md:text-xs px-2 md:px-0  md:font-bold">
+              {task_title}
+            </div>
+            <div className="text-[10px] md:text-xs px-2 md:px-0  opacity-50">
+              {current_date}
+            </div>
+          </div>
+        </div>
+      </td>
+      <td className="text-xs">
+        {buyerName}
+        <br />
+        <span className="text-[10px] md:text-xs px-2 md:px-0  opacity-50 ">
+          {buyerEmail}
+        </span>
+      </td>
+      <td className="text-[10px] px-2 md:px-0  md:text-xs">${perTaskAmount}</td>
+      {status === "pending" ? (
+  <td className="text-[10px] px-2 md:px-0 text-yellow-700 md:text-xs">
+    <span className="px-2 py-1 rounded-xl bg-yellow-100 ">{status}</span>
+  </td>
+) : status === "accept" ? (
+  <td className="text-[10px] px-2 md:px-0 text-green-700 md:text-xs">
+    <span className="px-2 py-1 rounded-xl bg-green-100 ">{status}</span>
+  </td>
+) : (
+  <td className="text-[10px] px-2 md:px-0 text-red-700 md:text-xs">
+    <span className="px-2 py-1 rounded-xl bg-red-100 ">{status}</span>
+  </td>
+)}
+    </tr>
+  );
+};
+
+export default WorkerSubmissionCard;
--- src/Pages/DashboardPages/WorkerPages/WorkerMySubmissions.jsx
+++ src/Pages/DashboardPages/WorkerPages/WorkerMySubmissions.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../../Components/Loading/Loading';
+import { AuthContext } from '../../../Authintication/AuthProvider/AuthProvider';
+import WorkerSubmissionCard from '../../../Components/WorkerComponents/WorkerSubmissionCard';
 
 const WorkerMySubmissions = () => {
+    const {user, loading} = useContext(AuthContext)
+
+
+    const axiosSecure = useAxiosSecure()
+    
+        const { data:mySubmissions=[], isPending ,refetch  } = useQuery({
+            queryKey: ['mySubmissions' ,user?.email], 
+            enabled:!loading,
+            queryFn: async() =>{
+                const {data} = await axiosSecure.get(`/apply/task/${user?.email}`)
+                return data
+            }
+          })
+          if(isPending) return <Loading/>
+
+
+
     return (
-        <div>
-            worker My Submissions
-        </div>
+        <div className="md:px-1 lg:px-16 px-0">
+      <div className="overflow-x-auto">
+        <table className="md:table">
+          {/* head */}
+          <thead >
+            <tr>
+              <th>Task info</th>
+              <th>Buyer info</th>
+              <th>Amount</th>  
+              <th>Status</th> 
+            </tr>
+          </thead>
+          <tbody className="">
+            {/* row 1 */}
+            {
+                mySubmissions.map(item=> <WorkerSubmissionCard key={item._id} refetch={refetch} item={item}/>)
+            }
+            
+            
+            {/* row 2 */}
+            
+          </tbody> 
+        </table>
+      </div>
+    </div>
     );
 };
 
